2390ff904534a025abcdcee319d89d86def62a75
Dan <dan@sumindex.com>
2026-05-08 13:36:06 +0530

    updated code.

--- docker/localstack/init/init-s3.sh
+++ docker/localstack/init/init-s3.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "LocalStack init complete. Terraform is responsible for provisioning S3 resources."
+set -e
+
+BUCKET_NAME="dispatch-bucket"
+
+awslocal s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null || \
+  awslocal s3api create-bucket --bucket "$BUCKET_NAME"
+
+awslocal s3api put-bucket-versioning \
+  --bucket "$BUCKET_NAME" \
+  --versioning-configuration Status=Enabled
+
+echo "LocalStack S3 bucket ready: $BUCKET_NAME"
